9b1a842a6ae5fb1be0781aa01c3c7ed863932c01
Sobhan <s.roghaniyan@gmail.com>
2026-08-20 00:49:27 +0330

    Changes in section exercises basic commands

--- navigation_basics/basic_commands/simple_practice.sh
+++ navigation_basics/basic_commands/simple_practice.sh
@@ -1,118 +1,50 @@
-
 sobhan@DESKTOP-DH3LD4L:~$ pwd
 /home/sobhan
-sobhan@DESKTOP-DH3LD4L:~$ mkdir my_project
-sobhan@DESKTOP-DH3LD4L:~$ cd my_project/
-sobhan@DESKTOP-DH3LD4L:~/my_project$ mkdir docs src backup
-sobhan@DESKTOP-DH3LD4L:~/my_project$ ls
-backup  docs  src
-sobhan@DESKTOP-DH3LD4L:~/my_project$ cd docs
-sobhan@DESKTOP-DH3LD4L:~/my_project/docs$ touch readme.txt
-sobhan@DESKTOP-DH3LD4L:~/my_project/docs$ echo "This is my project documentation" > readme.txt 
-sobhan@DESKTOP-DH3LD4L:~/my_project/docs$ cd ..
-sobhan@DESKTOP-DH3LD4L:~/my_project$ cd src
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ touch main.txt
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ echo "This is the main source file" > main.txt 
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ cat main.txt
-This is the main source file
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ cp main.txt ../backup/
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ touch temp.txt
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ mv temp.txt ../docs/
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ ls ../docs/
-readme.txt  temp.txt
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ cd .. 
-sobhan@DESKTOP-DH3LD4L:~/my_project$ cd .. 
-sobhan@DESKTOP-DH3LD4L:~$ cd my_project/
-sobhan@DESKTOP-DH3LD4L:~/my_project$ cd src/
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ ls
-main.txt
-sobhan@DESKTOP-DH3LD4L:~/my_project/src$ cd ..
-sobhan@DESKTOP-DH3LD4L:~/my_project$ ls -l
-total 12
-drwxrwxr-x 2 sobhan sobhan 4096 Jul 23 17:46 backup
-drwxrwxr-x 2 sobhan sobhan 4096 Jul 23 17:47 docs
-drwxrwxr-x 2 sobhan sobhan 4096 Jul 23 17:47 src
-sobhan@DESKTOP-DH3LD4L:~/my_project$ cd docs/
-sobhan@DESKTOP-DH3LD4L:~/my_project/docs$ ls -l
-total 4
--rw-rw-r-- 1 sobhan sobhan 33 Jul 23 17:33 readme.txt
--rw-rw-r-- 1 sobhan sobhan  0 Jul 23 17:47 temp.txt
-sobhan@DESKTOP-DH3LD4L:~/my_project/docs$ cat readme.txt 
-This is my project documentation
-sobhan@DESKTOP-DH3LD4L:~/my_project/docs$ cd ..
-sobhan@DESKTOP-DH3LD4L:~/my_project$ cd ..
-sobhan@DESKTOP-DH3LD4L:~$ tree my_project/
-my_project/
-├── backup
-│   └── main.txt
-├── docs
-│   ├── readme.txt
-│   └── temp.txt
-└── src
-    └── main.txt
-
-4 directories, 4 files
-sobhan@DESKTOP-DH3LD4L:~$ rm -f my_project/docs/temp.txt 
-sobhan@DESKTOP-DH3LD4L:~$ rm my_project/backup/main.txt 
-sobhan@DESKTOP-DH3LD4L:~$ rmdir my_project/backup/
-sobhan@DESKTOP-DH3LD4L:~$ ls -l my_
-my_first_backup.tar.gz  my_project/             
-sobhan@DESKTOP-DH3LD4L:~$ ls -l my_
-my_first_backup.tar.gz  my_project/             
-sobhan@DESKTOP-DH3LD4L:~$ ls -l my_project/
-total 8
-drwxrwxr-x 2 sobhan sobhan 4096 Jul 23 17:53 docs
-drwxrwxr-x 2 sobhan sobhan 4096 Jul 23 17:47 src
-sobhan@DESKTOP-DH3LD4L:~$ tree my_project/
-my_project/
-├── docs
-│   └── readme.txt
-└── src
-    └── main.txt
-
-3 directories, 2 files
-sobhan@DESKTOP-DH3LD4L:~$ ls -R
-.:
-backup_test  go_to_work.sh  my_first_backup.tar.gz  my_project  script.sh  workspace
-
-./backup_test:
-workspace
-
-./backup_test/workspace:
-archive      archive_old  file.vi            my_archive.tar             note.txt   server.conf
-archive.zip  file.nano    linux_wiki.kernel  my_archive_compressed.tar  script.sh
-
-./backup_test/workspace/archive:
-backup.txt  main_note.txt
-
-./backup_test/workspace/archive_old:
-backup.txt  main_note.txt
-
-./my_project:
-docs  src
-
-./my_project/docs:
-readme.txt
-
-./my_project/src:
-main.txt
-
-./workspace:
-archive      file.nano  hard_link.txt      my_archive.tar             script.sh    soft_link.txt
-archive.zip  file.vi    linux_wiki.kernel  my_archive_compressed.tar  server.conf
-archive_old  hard.txt   main_file.txt      note.txt                   soft.txt
-
-./workspace/archive:
-backup.txt  main_note.txt
-
-./workspace/archive_old:
-backup.txt  main_note.txt
-sobhan@DESKTOP-DH3LD4L:~$ ls -R my_project/
-my_project/:
-docs  src
-
-my_project/docs:
-readme.txt
-
-my_project/src:
-main.txt
+sobhan@DESKTOP-DH3LD4L:~$ cd /
+sobhan@DESKTOP-DH3LD4L:/$ pwd
+/
+sobhan@DESKTOP-DH3LD4L:/$ ls -la
+total 2900
+drwxr-xr-x  34 root root    4096 Jun 26 10:56 .
+drwxr-xr-x  34 root root    4096 Jun 26 10:56 ..
+lrwxrwxrwx   1 root root       7 Apr 20 12:16 bin -> usr/bin
+drwxr-xr-x   2 root root    4096 Apr 20 12:16 boot
+drwxr-xr-x  15 root root    3840 Jun 26 10:56 dev
+drwxr-xr-x  89 root root    4096 Jun 26 10:56 etc
+drwxr-xr-x   3 root root    4096 Jun 12 19:10 home
+-rwxr-xr-x   1 root root 2836528 May 19 19:03 init
+lrwxrwxrwx   1 root root       7 Apr 20 12:16 lib -> usr/lib
+lrwxrwxrwx   1 root root       9 Apr 20 12:16 lib64 -> usr/lib64
+drwx------   2 root root   16384 Jun 12 19:09 lost+found
+drwxr-xr-x   2 root root    4096 Apr 20 21:35 media
+drwxr-xr-x   6 root root    4096 Jun 12 19:09 mnt
+drwxr-xr-x   2 root root    4096 Apr 20 21:35 opt
+dr-xr-xr-x 225 root root       0 Jun 26 10:56 proc
+drwx------   3 root root    4096 Apr 20 21:37 root
+drwxr-xr-x  23 root root     600 Jun 26 10:56 run
+lrwxrwxrwx   1 root root       8 Apr 20 12:16 sbin -> usr/sbin
+drwxr-xr-x   2 root root    4096 Jun 12 19:09 snap
+drwxr-xr-x   2 root root    4096 Apr 20 21:35 srv
+dr-xr-xr-x  13 root root       0 Jun 26 10:54 sys
+drwxrwxrwt   8 root root     160 Jun 26 11:58 tmp
+drwxr-xr-x  12 root root    4096 Apr 20 21:35 usr
+drwxr-xr-x  13 root root    4096 Jun 12 19:09 var
+drwx------   2 root root    4096 Jun 13 20:28 wslABpMEG
+drwx------   2 root root    4096 Jun 26 10:00 wslBFdNNK
+drwx------   2 root root    4096 Jun 26 10:00 wslBKmoMP
+drwx------   2 root root    4096 Jun 26 10:00 wslCCIcJP
+drwx------   2 root root    4096 Jun 26 10:00 wslDIIcMP
+drwx------   2 root root    4096 Jun 13 20:28 wslFEABEG
+drwx------   2 root root    4096 Jun 26 10:00 wslHANbHO
+drwx------   2 root root    4096 Jun 26 10:00 wslIkPhLP
+drwx------   2 root root    4096 Jun 13 20:28 wslMcbbkE
+drwx------   2 root root    4096 Jun 26 10:00 wslcojoMK
+drwx------   2 root root    4096 Jun 26 10:00 wslfejaJJ
+drwx------   2 root root    4096 Jun 26 10:00 wslhAmdNK
+drwx------   2 root root    4096 Jun 13 20:28 wsliaCNBG
+drwx------   2 root root    4096 Jun 26 10:00 wslkikGLK
+drwx------   2 root root    4096 Jun 13 20:28 wslnlkbEG
+sobhan@DESKTOP-DH3LD4L:/$ cd
+sobhan@DESKTOP-DH3LD4L:~$ pwd
+/home/sobhan
+sobhan@DESKTOP-DH3LD4L:~$
